cecbf3584471e312812e3fe8ef517ec0029a8497
NJKatorz <nathanesakwa@gmail.com>
2023-01-19 19:03:28 +0100

    refactor(addexoplanet): optimization of the function : addExoplanet

--- src/addExoplanet.js
+++ src/addExoplanet.js
@@ -1,6 +1,4 @@
 module.exports = function (name) {
         const regResults = new RegExp("^[A-Z0-9 .-]+$");
-        if (regResults.test(name))
-            return true;
-        return false;
+        return regResults.test(name);
 };
